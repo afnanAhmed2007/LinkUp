4e2281861a30c3ba9589ac771a24fa6609147e35
Afnan <afnanahmed4847@gmail.com>
2026-05-20 20:58:22 -0400

    new

--- vite-frontend/src/LoginUI.tsx
+++ vite-frontend/src/LoginUI.tsx
@@ -3,7 +3,7 @@
 function LoginUI() {
     return (
         <div className="login-container">
-            <h1 className="login-title">LinkUp</h1>
+            <h1 className="login-title" style={{paddingTop: "10%"}}>LinkUp</h1>
             <h1 className="login-title">Enter Email to Continue:</h1>
             <h1 className="login-title">Continue with Google:</h1>
             <h1 className="login-title">Don't have an account?</h1>
